1b0939522ea49b5b25cc443f4b934d13e2a05206
ChristianTongCruz <christian.tongcruz96@outlook.com>
2023-05-26 09:32:33 -0500

    Add Guia

--- src/app/modules/guias/components/add-guia/add-guia.component.ts
+++ src/app/modules/guias/components/add-guia/add-guia.component.ts
@@ -28,21 +28,33 @@ export class AddGuiaComponent implements OnInit {
 
   /**INICIALICE */
 
-  DNI: any;
+  DNI: any = {};
+  FullName?: string;
 
   /**LIFE COMPONENT */
 
   ngOnInit() {}
 
-  ConsultarDni(dni: any) {
-    return this._DniRucService
-      .getDni(dni)
-      .subscribe((response) => (response = this.DNI));
-  }
+  /**FORMS GROUPS*/
 
-  NombreDni(){
-
-  }
+  FormGuiaList = this._FormBuilder.group({
+    operacion: [''],
+    peso_bruto_total: [''],
+    peso_bruto_unidad_medida: [''],
+    punto_partida_direccion: [''],
+    codigo_partida_establecimiento_sunat: [''],
+    punto_llegada_direccion: [''],
+    codigo_llegada_establecimiento_sunat: [''],
+    observaciones: [''],
+    enviar_cliente: [''],
+    formato_pdf: [''],
+    tipo_comprobante: [''],
+    destinatario: [''],
+    cliente: [''],
+    placa: [''],
+    punto_partida: [''],
+    punto_llegada: [''],
+  });
 
   FormDatosVehiculo = this._FormBuilder.group({
     placa: [''],
@@ -61,15 +73,32 @@ export class AddGuiaComponent implements OnInit {
 
   FormDatosDestinatario = this._FormBuilder.group({
     numero_documento: [''],
-    nombre: ['', Validators.required],
+    nombre: [''],
     tipo_documento: [''],
   });
 
-  get numero_documento() {
+  /**FORMS METHODS */
+
+  get FormDatosTransportista_numero_documento() {
     return this.FormDatosTransportista.get('numero_documento') as FormControl;
   }
 
+  /**METHODS */
+
+  mostrarDNI(dni: any) {
+    return this._DniRucService.getDni(dni).subscribe((response) => {
+      this.DNI = response;
+      this.FullName = `${this.DNI.nombres} ${this.DNI.apellidoPaterno} ${this.DNI.apellidoMaterno}`;
+      console.log(this.DNI);
+    });
+  }
+
+  consultarDNI() {
+    const dni = this.FormDatosTransportista_numero_documento.value;
+    this.mostrarDNI(dni);
+  }
+
   proces() {
-    console.log('this.FormDatosTransportista.value');
+    console.log(this.FormDatosTransportista_numero_documento.value);
   }
 }
